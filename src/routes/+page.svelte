481695abb8718a15b9b76e8863e0aed66dea641c
Mads Westberg <mads.westberg@oyorooms.com>
2022-12-15 15:35:45 +0100

    add image data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<h1>This text needs to replaced with a component showing Airbnb style image grid</h1>
+<p>See <a href="https://www.airbnb.com/rooms/20238974">the link</a> for an example. The ideal component will make use of Flowbite-Svelte and Tailwind CSS</p>
+<p>The grid should be dynamic and interactive and work for multiple screen sizes similiar to the one on Airbnb</p>
+<p>Example images can be found in image_data.json</p>
+

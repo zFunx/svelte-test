617fe175594a49359bc7f19fea4c3b7288a1859b
zFunx <20762882+zFunx@users.noreply.github.com>
2022-12-19 06:12:18 +0530

    Completed image grid

---
+++ src/lib/Components/ImageCollection/ImageGrid/CloseButton.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let className ='';
+
+    import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+    const onClick = () => dispatch('click')
+</script>
+
+<button on:click={onClick} class={`bg-white hover:bg-slate-100 px-3 py-2 rounded-full flex items-center text-sm font-medium ${className}`}>
+    <!-- Icon from https://heroicons.dev/ -->
+    <svg class="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    Close
+</button>
--- src/lib/Components/ImageCollection/ImageGrid/ImageGrid.svelte
+++ src/lib/Components/ImageCollection/ImageGrid/ImageGrid.svelte
@@ -3,17 +3,11 @@
 
 	// Components
 	import ShowAllBtn from '$lib/Components/ImageCollection/ImageGrid/ShowAllBtn.svelte';
+	import CloseButton from './CloseButton.svelte';
 
 	// Expand/collapse grid
 	let expandImageGrid = false;
 	const toggleImageGridExpansion = () => (expandImageGrid = !expandImageGrid);
-
-	// Modify images with spans
-	images = images.map((img, index) => ({
-		...img,
-		rowSpan: index % 3 == 0 ? 4 : 2,
-		colSpan: index % 3 == 0 ? 4 : 2
-	}));
 
 	let activeIndex = -1;
 	const setActiveIndex = (index) => (activeIndex = index);
@@ -22,24 +16,35 @@
 <div
 	class={`${
 		expandImageGrid
-			? 'bg-white pt-16 pb-4 px-12 top-0 bottom-0 left-0 right-0 overflow-auto fixed'
-			: 'my-4 mx-12 overflow-hidden relative'
-	} rounded-lg`}
+			? 'bg-white md:pt-24 md:pb-4 md:px-12 top-0 bottom-0 left-0 right-0 overflow-auto fixed'
+			: 'my-4 mx-12 overflow-hidden relative rounded-lg'
+	} `}
 >
 	<div class="grid grid-cols-4 gap-2 grid-flow-dense">
 		{#each images.slice(0, expandImageGrid ? images.length : 5) as image, index (image.image_url)}
 			<img
-				class={`h-full object-cover transition-all brightness-100 border-white cursor-pointer hover:brightness-90
-                    ${'row-span-' + [expandImageGrid ? image.rowSpan : 0 == index ? 2 : 1]}
-                    ${'col-span-' + [expandImageGrid ? image.colSpan : 0 == index ? 2 : 1]}`}
+				class={`${
+					index % 3 == 0 && expandImageGrid ? 'w-full' : 'h-full'
+				} object-cover transition-all brightness-100 border-white cursor-pointer hover:brightness-90
+                    ${
+											'row-span-' +
+											[expandImageGrid ? (index % 3 == 0 ? 4 : 2) : 0 == index ? 2 : 1]
+										}
+                    ${
+											'col-span-' +
+											[expandImageGrid ? (index % 3 == 0 ? 4 : 2) : 0 == index ? 2 : 1]
+										}`}
 				src={image.image_url}
 				alt={image.tags}
-				on:click={setActiveIndex(index)}
+				on:click={expandImageGrid ? setActiveIndex(index) : toggleImageGridExpansion()}
+				loading="lazy"
 			/>
 		{/each}
 
 		{#if !expandImageGrid}
 			<ShowAllBtn className="absolute right-6 bottom-6" on:click={toggleImageGridExpansion} />
+		{:else}
+			<CloseButton className="fixed top-6 left-4 md:left-8" on:click={toggleImageGridExpansion} />
 		{/if}
 	</div>
 </div>
